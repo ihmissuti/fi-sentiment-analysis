6924ac4c70a43ec196815a11b785e1f5bb8ed287
Kimmo Ihanus <kimmo@ihmissuti.fi>
2018-06-13 13:36:11 +0000

    html

--- server.js
+++ server.js
@@ -1,15 +1,51 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
 var Sentiment = require('sentiment');
 var sentiment = new Sentiment();
 const fs = require('fs');
 
-var fiLabels = JSON.parse(fs.readFileSync('fiLabels.json', 'utf8'));
+app.use(express.static(__dirname + '/public'));
 
-var fiLanguage = {
-    labels: fiLabels
-};
+const server = app.listen(process.env.PORT || 8080, () => {
+    console.log('Express server listening on port %d', server.address().port);
+});
 
-sentiment.registerLanguage('fi', fiLanguage);
+const io = require('socket.io')(server);
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
 
-var input = 'Olen iloinen testilause.';
-var result = sentiment.analyze(input, { language: 'fi' });
-console.dir(result);
+app.get('/', (req, res) => {
+  res.sendFile('index.html');
+});
+
+io.on('connection', function(socket) {
+    
+    socket.on('message', (text) => {
+        console.log(text)
+      
+        var fiLabels = JSON.parse(fs.readFileSync('fiLabels.json', 'utf8'));
+
+        var fiLanguage = {
+            labels: fiLabels
+        };
+        
+        sentiment.registerLanguage('fi', fiLanguage);
+        
+        var result = sentiment.analyze(text, { language: 'fi' });
+        console.dir(result);
+        
+        if (result.score > 0) {
+            var mood = "positiivinen"
+        } else if  (result.score < 0) {
+            var mood = "negatiivinen"
+        } else {
+            var mood = "neutraali"
+        }
+        
+        socket.emit('mood', mood);
+        socket.emit('score', result.score);
+  });
+});
+
