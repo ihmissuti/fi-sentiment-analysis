aa402e001f9c10f280680d8d3dcf7ee2935fe8cb
Kimmo Ihanus <kimmo@ihmissuti.fi>
2018-06-14 04:25:05 +0000

    mongodb added

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ var Sentiment = require('sentiment');
 var sentiment = new Sentiment();
 const fs = require('fs');
 var mongoose = require('mongoose');
+var moment = require('moment');
 mongoose.connect(process.env.MONGODB_URI);
+
+var now = moment().format('DD-MM-YYYY');
 
 if (process.env.MONGODB_URI) {
     //connect to Mongo DB
@@ -15,6 +18,31 @@ if (process.env.MONGODB_URI) {
       // we're connected!
       console.log("connected to Mongo DB")
     });
+    
+    var sentimentSchema = mongoose.Schema({
+      labels: [
+        {
+          text: String, 
+          sentiment: String,
+          score: Number,
+          processed: Number,
+          _id : false
+        }
+      ],
+      id: String,
+      sentiment: String,
+      score: Number,
+      processed: Number
+    });
+    
+    var labelsSchema = mongoose.Schema({
+        id: Number,
+        labels: Object
+    })
+    
+    
+    var inputStorage = mongoose.model('sentiment', sentimentSchema);
+    var labelsStorage = mongoose.model('label', labelsSchema);
 }
 
 app.use(express.static(__dirname + '/public'));
@@ -36,27 +64,88 @@ io.on('connection', function(socket) {
     
     socket.on('message', (text) => {
         console.log(text)
-      
-        var fiLabels = JSON.parse(fs.readFileSync('fiLabels.json', 'utf8'));
+        
+        if (process.env.MONGODB_URI) {
 
-        var fiLanguage = {
-            labels: fiLabels
-        };
+            labelsStorage.findOne({id: 1}, 'labels', function(err, doc){
+
+                if(err){
+                  console.log(err);
+                } else{
+
+                    var fiLabels = doc.labels;
+                    
+                     var fiLanguage = {
+                        labels: fiLabels
+                    };
+                    
+                    sentiment.registerLanguage('fi', fiLanguage);
+                    
+                    var result = sentiment.analyze(text, { language: 'fi' });
+                    console.dir(result);
+                    
+                    if (result.score > 0) {
+                        var mood = "positiivinen"
+                        var moodEn = "positive"
+                    } else if  (result.score < 0) {
+                        var mood = "negatiivinen"
+                        var moodEn = "negative"
+                    } else {
+                        var mood = "neutraali"
+                        var moodEn = "neutral"
+                    }
+                    
+                    socket.emit('mood', mood);
+                    socket.emit('score', result.score);
+                    
+                    if (process.env.MONGODB_URI) {
+                        var labelsArrays = {"text": text, "sentiment": moodEn, "score": result.score, "processed": 0};
+                        inputStorage.findOneAndUpdate({ 'id': now }, {$push: {labels: labelsArrays}},  { upsert: true, new: true }, function(error, res) {
+                            if (error) {
+                              console.log(error)
+                            }
+                            
+                            console.log("DB unknown update:")
+                            console.log(res)
+                    
+                          })
+                        
+                    }
+                    
+                   
+                }
+            })
         
-        sentiment.registerLanguage('fi', fiLanguage);
-        
-        var result = sentiment.analyze(text, { language: 'fi' });
-        console.dir(result);
-        
-        if (result.score > 0) {
-            var mood = "positiivinen"
-        } else if  (result.score < 0) {
-            var mood = "negatiivinen"
         } else {
-            var mood = "neutraali"
+
+            console.log("No mongo DB url. Use the local storage")
+            
+            var fiLabels = JSON.parse(fs.readFileSync('fiLabels.json', 'utf8'));
+            
+            var fiLanguage = {
+                labels: fiLabels
+           };
+            
+            sentiment.registerLanguage('fi', fiLanguage);
+                    
+            var result = sentiment.analyze(text, { language: 'fi' });
+            console.dir(result);
+                    
+            if (result.score > 0) {
+                var mood = "positiivinen"
+                var moodEn = "positive"
+            } else if  (result.score < 0) {
+                var mood = "negatiivinen"
+                var moodEn = "negative"
+            } else {
+                var mood = "neutraali"
+                var moodEn = "neutral"
+            }
+                    
+            socket.emit('mood', mood);
+            socket.emit('score', result.score);
         }
-        
-        socket.emit('mood', mood);
-        socket.emit('score', result.score);
+
+
   });
 });
